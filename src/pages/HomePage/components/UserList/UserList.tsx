fbc542f6b6e1c1a49ab5862a9f19a0b5c2f92a8a
yuliya-karimova <y.o.karimova@gmail.com>
2022-04-28 17:29:43 +0300

    feat: add user count

--- src/pages/HomePage/components/UserList/UserList.tsx
+++ src/pages/HomePage/components/UserList/UserList.tsx
@@ -6,17 +6,20 @@ import { UserType } from '../../../../globalTypes';
 import styles from './UserList.module.scss';
 
 type PropsType = {
-  userList: UserType[];
+  userList: UserType[] | null;
 };
 
 const UserList = React.memo(function UserList({ userList }: PropsType) {
   return (
     <>
       {userList ? (
-        <div className={styles.list}>
-          {userList.map((user) => (
-            <UserCard key={user.id} userInfo={user} />
-          ))}
+        <div className={styles.wrapper}>
+          <div className={styles.list}>
+            {userList.map((user) => (
+              <UserCard key={user.id} userInfo={user} />
+            ))}
+          </div>
+          <p className={styles.totalCount}>Найдено {userList.length} пользователей</p>
         </div>
       ) : (
         <p>Sorry, no users</p>
