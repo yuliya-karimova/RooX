8981c17730d1bf88180e34a67dba1a4875390871
yuliya-karimova <y.o.karimova@gmail.com>
2022-04-28 09:26:29 +0300

    refactor: create sorting function

--- src/pages/HomePage/helpers/getUserList.ts
+++ src/pages/HomePage/helpers/getUserList.ts
@@ -1,25 +1,25 @@
 import { fetchUserList } from '../../../api/requests';
 import { SortTypes, UserType } from '../../../globalTypes';
 
+const sortByName = (aName: string, bName: string) => {
+  if (aName === bName) {
+    return 0;
+  }
+
+  return aName < bName ? -1 : 1;
+};
+
 const getSortedUserList = (userList: UserType[], sortType: SortTypes) => {
   if (sortType === SortTypes.city) {
-    return userList.sort(({ address: { city: aName } }, { address: { city: bName } }) => {
-      if (aName === bName) {
-        return 0;
-      }
-
-      return aName < bName ? -1 : 1;
-    });
+    return userList.sort(({ address: { city: aName } }, { address: { city: bName } }) =>
+      sortByName(aName, bName)
+    );
   }
 
   if (sortType === SortTypes.company) {
-    return userList.sort(({ company: { name: aName } }, { company: { name: bName } }) => {
-      if (aName === bName) {
-        return 0;
-      }
-
-      return aName < bName ? -1 : 1;
-    });
+    return userList.sort(({ company: { name: aName } }, { company: { name: bName } }) =>
+      sortByName(aName, bName)
+    );
   }
 
   return userList;
